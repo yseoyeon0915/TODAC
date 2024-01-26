2d12512e74b8d20002bd11c531ada67241543e01
yseoyeon0915 <yseoyeon0915@gmail.com>
2024-01-26 16:12:21 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import mainImg from './image/c5.png';
+
+function App(props) {
+  return (
+    <div className="App">
+      <img alt='' src={mainImg}/>
+      <h2><b>스프링부트+리액트 CI/CD 성공기원!!! <br/>한번에 성공하기</b></h2>
+    </div>
+  );
+}
+
+export default App;
